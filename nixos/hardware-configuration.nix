a6c5e2c4429aeef48e6f17a6f0e366563cffca7e
Mia <miabernhardt75@gmail.com>
2024-03-01 16:28:48 +0100

    added kernel parameter for QEMU

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -8,10 +8,14 @@
     [ (modulesPath + "/profiles/qemu-guest.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "ahci" "sd_mod" "sr_mod" "virtio_net" ];
+  boot.initrd.availableKernelModules = [ "ahci" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
+
+  boot.kernelParams = [
+    ''"console=ttyS0"''
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
