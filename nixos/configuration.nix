58d792851a8c6317183e95ac58edf99c8910fe14
Mia <miabernhardt75@gmail.com>
2024-03-12 19:28:24 +0100

    added grub theme

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -23,7 +23,12 @@ in
 
   # Bootloader.
   boot.loader.grub.enable = true;
-
+  boot.loader.grub.theme = pkgs.stdenv.mkDerivation {
+    pname = "catppuccin-mocha-grub";
+    version = "1.0";
+    src = ./resources/catppuccin-mocha-grub;
+    installPhase = "cp -r . $out";
+  };
 
   # Home Manager
   programs.fuse.userAllowOther = true;
