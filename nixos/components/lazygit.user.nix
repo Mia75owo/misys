a91e24aa02c80a790061b9221ff8743b9e9787cb
Mia <miabernhardt75@gmail.com>
2024-03-12 18:51:41 +0100

    fixed error in lazygit config

--- nixos/components/lazygit.user.nix
+++ nixos/components/lazygit.user.nix
@@ -9,8 +9,4 @@
       (builtins.fromJSON (builtins.readFile ../resources/catppuccin-mocha-lazygit.json))
     ]);
   };
-
-  home.file."${config.xdg.configHome}/lazygit/themes/catppuccin-mocha.yml" = {
-    source = ../resources/catppuccin-mocha-lazygit.yml;
-  };
 }
