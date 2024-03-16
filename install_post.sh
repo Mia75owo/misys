59920fed3979cbf4b07cf3b145bd15424e13b8a1
Mia <miabernhardt75@gmail.com>
2024-03-16 19:02:26 +0100

    updated install_post.sh

--- install_post.sh
+++ install_post.sh
@@ -2,4 +2,5 @@ if [ ! -d "/persist/home" ]; then
   sudo mkdir /persist/home
   sudo chmod -R $(id -u):$(id -g)
   sudo chown -R $(id -un) /persist/misys
+  sudo chown -R $(id -un) /persist/home
 fi
